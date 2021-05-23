c3dbeb022901baec30af7521c79ed396854c2be1
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-05-22 19:44:52 -0600

    added svg icons

--- src/components/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,9 +1,9 @@
 import styles from './drawer-toggle.module.css';
 const DrawerToggle = (props) => (
     <div className={styles.drawerToggle} onClick={props.clicked}>
-        <div></div>
-        <div></div>
-        <div></div>
+        <svg style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
+            <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+        </svg>
     </div>
 );
 export default DrawerToggle;
--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -13,7 +13,11 @@ const SideDrawer = (props) => {
                     <div className={styles.logo}>
                         <Logo />
                     </div>
-                    {props.open ? <span className={styles.closeButton} onClick={props.clicked}></span> : null}
+                    {props.open ?
+                        <svg className={styles.closeButton} style={{ width: '24px', height: '24px' }} onClick={props.clicked} viewBox="0 0 24 24">
+                            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+                        </svg>
+                        : null}
                 </div>
                 <nav>
                     <NavigationItems />
